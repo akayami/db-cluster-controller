8b7f663dfb710c2eeb0feeac67b2df81ff6b6ea2
Tomasz Rakowski <akayami@gmail.com>
2017-10-12 17:00:30 -0400

    Initial version

---
+++ index.js
@@ -0,0 +1,129 @@
+"use strict";
+const async = require('async');
+const clone = require('clone');
+
+module.exports = function(clusterConfig, structureString, dataArray) {
+
+	let cfg = {
+		adapter: clusterConfig.adapter,
+		driver: clusterConfig.driver,
+		global: clone(clusterConfig.global),
+		pools: clusterConfig.pools
+	};
+
+	let dbname = cfg.global.database;
+	delete cfg.global.database;
+
+	let cluster, cluster_real, internalConn, realConn;
+
+	return {
+		setup: function(cb) {
+			cluster = require('db-cluster')(cfg);
+			cluster_real = require('db-cluster')(clusterConfig);
+			cluster.master(function(err, conn) {
+				if (err) {
+					return cb(err);
+				}
+				internalConn = conn;
+
+				let tasks = [
+					function(asyncCB) {
+						asyncCB(null, conn);
+					},
+					function(conn, asyncCB) {
+						conn.query('DROP DATABASE IF EXISTS ??', [dbname], function(err, result) {
+							if (err) {
+								asyncCB(err);
+							} else {
+								asyncCB(null, conn);
+							}
+						})
+					},
+					function(conn, asyncCB) {
+						conn.query('CREATE DATABASE ??', [dbname], function(err, result) {
+							if (err) {
+								asyncCB(err);
+							} else {
+								asyncCB(null, conn, result);
+							}
+						})
+					},
+					function(conn, result, asyncCB) {
+						conn.query('USE ??', [dbname], function(err, result) {
+							if (err) {
+								asyncCB(err);
+							} else {
+								asyncCB(null, conn, result);
+
+							}
+						})
+					}
+				];
+
+				async.waterfall(tasks, function(err, result) {
+					if(err) {
+						return cb(err);
+					}
+					cluster_real.master(function(err, conn) {
+						if (err) {
+							return cb(err);
+						}
+						realConn = conn;
+						//console.log(realConn);
+						let mocker = require('mock-db-generator')(
+							realConn,
+							dbname,
+							String(structureString).split(';')
+						);
+						if(dataArray) {
+							mocker.make(dataArray);
+						}
+						mocker.run(function(err) {
+							cb(err);
+						})
+					})
+				})
+			});
+		},
+		shutdown: function(cb, wrapper) {
+			realConn.release(function(err) {
+				cluster.end(function(err) {
+					if(err) {
+						return cb(err);
+					}
+					internalConn.release(function() {
+						cluster_real.end(function(err) {
+							cb(err);
+						})
+					})
+				})
+			})
+		},
+
+		teardown: function(cb, wrapper) {
+			if(!realConn) {
+				return cb();
+			}
+			realConn.release(function (e) {
+				cluster_real.end(function (err) {
+					if (err) {
+						return cb(err);
+					}
+					if (e) {
+						return cb(e);
+					}
+					internalConn.query('DROP DATABASE IF EXISTS ??', [dbname], function (err, result) {
+						if (err) {
+							return cb(err);
+						}
+						internalConn.release(function () {
+							cluster.end(function (err) {
+								return cb(err);
+							})
+						})
+					});
+				})
+			})
+		}
+	}
+};
---
+++ test/index.js
@@ -0,0 +1,84 @@
+"use strict";
+const path = require('path');
+
+let cluster = {
+	adapter: require('db-cluster-mysql'),
+	driver: require('mysql'),
+	global: {
+		user: 'root',
+		database: "db-cluster-controller-test-" + process.pid
+	},
+	pools: {
+		master: {},
+		slave: {}
+	}
+};
+let ctrl;
+
+describe('Basic tests', function () {
+
+	this.timeout(10000);
+
+
+	it('Needs to return ctrl object', (done) => {
+		ctrl = require('../index')(
+			cluster,
+			path.resolve(__dirname,'/structure.sql')
+		);
+		if(ctrl.setup && ctrl.shutdown && ctrl.teardown) {
+			done();
+		} else {
+			done('Unexpected response');
+		}
+	});
+
+	it('Needs to create database', (done) => {
+
+		let structure = require('fs').readFileSync(__dirname + '/structure.sql').toString();
+
+		ctrl = require('../index')(
+			cluster,
+			structure
+		);
+		ctrl.setup((e) => {
+			done(e);
+		});
+	});
+
+	it('Needs to create database and teardown the cluster', (done) => {
+
+		let structure = require('fs').readFileSync(__dirname + '/structure.sql').toString();
+
+		ctrl = require('../index')(
+			cluster,
+			structure
+		);
+		ctrl.setup((e) => {
+			if(e) {
+				return done(e);
+			}
+			ctrl.teardown((e) => {
+				done(e);
+			})
+
+		});
+	});
+
+	afterEach((done) => {
+		if(ctrl.teardown) {
+			ctrl.teardown((e) => {
+				if(e) {
+					if (e.message === 'Cannot enqueue Query after invoking quit.') {
+						done();
+					} else {
+						done(e);
+					}
+				} else {
+					done();
+				}
+			})
+		} else {
+			done();
+		}
+	})
+});
